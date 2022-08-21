d99ab877b8d6f9b34b9f73ce3883349d8c10e19a
tem12qaz <tem12qaz@yandex.ru>
2022-08-21 21:54:14 +0300

    inti

--- models.py
+++ models.py
@@ -1,6 +1,5 @@
 from tortoise.models import Model
 from tortoise import fields
-from flask_security import UserMixin, RoleMixin
 
 
 class TelegramUser(Model):
@@ -25,19 +24,3 @@ class Keyword(Model):
     id = fields.IntField(pk=True)
     owner = fields.ForeignKeyField('models.TelegramUser', related_name='keywords', index=True)
     text = fields.CharField(100)
-
-
-class User(Model, UserMixin):
-    id = fields.IntField(pk=True)
-    email = fields.CharField(254, unique=True)
-    password = fields.CharField(255)
-    active = fields.BooleanField()
-    roles = fields.ManyToManyField(
-        'models.Role', related_name='users', through='roles_users'
-    )
-
-
-class Role(Model, RoleMixin):
-    id = fields.IntField(pk=True)
-    name = fields.CharField(100, unique=True)
-    description = fields.CharField(255)
